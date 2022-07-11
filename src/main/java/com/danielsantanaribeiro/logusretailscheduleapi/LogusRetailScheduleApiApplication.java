c8c921059aae6674b9d45aee6040b5aaed95cea7
Daniel Santana Ribeiro <danielsantanaribeiro2015@gmail.com>
2022-07-11 16:40:41 -0300

    Initial commit

---
+++ src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/LogusRetailScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package com.danielsantanaribeiro.logusretailscheduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogusRetailScheduleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogusRetailScheduleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielsantanaribeiro/logusretailscheduleapi/LogusRetailScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsantanaribeiro.logusretailscheduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogusRetailScheduleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
