8f48f6e75cf3f19d026690bb53b908aff612645a
Daniel Santana Ribeiro <danielsantanaribeiro2015@gmail.com>
2022-07-12 12:46:40 -0300

    Adding initial Schedule repository

---
+++ src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/repository/ScheduleRepository.java
@@ -0,0 +1,11 @@
+package com.danielsantanaribeiro.logusretailscheduleapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danielsantanaribeiro.logusretailscheduleapi.model.Schedule;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long>{
+
+}
