60a0dc15a7922c066a30e44f24bf773b9558ec64
Daniel Santana Ribeiro <danielsantanaribeiro2015@gmail.com>
2022-07-12 12:58:00 -0300

    Modifying method to return all the schedules through findAll ScheduleRepository method

--- src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/controller/ScheduleController.java
+++ src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/controller/ScheduleController.java
@@ -1,22 +1,34 @@
 package com.danielsantanaribeiro.logusretailscheduleapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.danielsantanaribeiro.logusretailscheduleapi.model.Schedule;
+import com.danielsantanaribeiro.logusretailscheduleapi.repository.ScheduleRepository;
+
 @RestController
 @RequestMapping("/schedules")
 public class ScheduleController {
 	
+	@Autowired
+	private ScheduleRepository scheduleRepository;
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Olá, mundo!";
+	public List<Schedule> scheduleList() {
+		return scheduleRepository.findAll();		
 	}
 
 	@RequestMapping(method = RequestMethod.GET, params = "nome")
-	public String listar(@RequestParam(value = "nome") String nome) {
+	public String listar(@RequestParam(value = "nome") String nome) {		
 		return "Olá, " + nome;
 	}
-
+	
+	
+	
+	
 }
