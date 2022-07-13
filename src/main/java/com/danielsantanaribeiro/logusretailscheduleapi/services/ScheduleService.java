4ba3c7b2bca1d61acdc9b6bf6b627a10d192ac17
Daniel Santana Ribeiro <danielsantanaribeiro2015@gmail.com>
2022-07-13 19:38:08 -0300

    Fix ScheduleService - Sort by localdate and localtime; adjusting methods according to repository

--- src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/services/ScheduleService.java
+++ src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/services/ScheduleService.java
@@ -1,12 +1,12 @@
 package com.danielsantanaribeiro.logusretailscheduleapi.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.danielsantanaribeiro.logusretailscheduleapi.model.Schedule;
@@ -14,23 +14,25 @@ import com.danielsantanaribeiro.logusretailscheduleapi.repository.ScheduleReposi
 
 @Service
 public class ScheduleService {
-	
+
 	@Autowired
 	private ScheduleRepository scheduleRepository;
-	
-	public List<Schedule> findAll(){
-		//need to return orderByScheduleDateTime
-		return scheduleRepository.findAll(Sort.by(Direction.ASC, "scheduleDatetime"));
+
+	public List<Schedule> findAll() {
+		// need to return orderByScheduleDateTime
+		return scheduleRepository.findAll(Sort.by(Direction.ASC, "scheduleDate")
+				.and(Sort.by(Direction.ASC, "scheduleTime")));	
 	}
-	
-	public Schedule findById(Long id) {	
-		Optional<Schedule> obj = scheduleRepository.findById(id); 
+
+	public Schedule findById(Long id) {
+		Optional<Schedule> obj = scheduleRepository.findById(id);
 		return obj.orElse(null);
 	}
-	
+
 	public Schedule save(Schedule newSchedule) {
 		newSchedule.setId(null);
-		//Cannot save a schedule for a patient who already have schedule on the new schedule date 
+		// Cannot save a schedule for a patient who already have schedule on the new
+		// schedule date
 		/*
 		 * SimpleDateFormat ymdFormat = new SimpleDateFormat("yyyy-MM-dd"); String
 		 * ymdScheduleDate = ymdFormat.format(newSchedule.getScheduleDatetime());
@@ -40,11 +42,18 @@ public class ScheduleService {
 		 */
 		return scheduleRepository.save(newSchedule);
 	}
+
+	public List<Schedule> findByPatientNameAndDate(String patientName, LocalDate date) {
+		return scheduleRepository.findByPatientNameAndScheduleDate(patientName, date);
+	}
 	
-	/*
-	 * public List<Schedule> findByNameAndDate(String patientName, String
-	 * scheduleDate) { return scheduleRepository.findByNameAndDate(patientName,
-	 * scheduleDate); }
-	 */
+	public List<Schedule> findByPatientName(String patientName) {
+		return scheduleRepository.findByPatientName(patientName);
+	}
+	
+	public List<Schedule> findByDate(LocalDate date) {
+		return scheduleRepository.findByScheduleDate(date);	
+	}
+	
 
 }
