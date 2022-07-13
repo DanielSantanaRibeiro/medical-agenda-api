7b01cb22b344e305c064486dc213abc4849639ea
Daniel Santana Ribeiro <danielsantanaribeiro2015@gmail.com>
2022-07-13 19:35:45 -0300

    Fixing Schedule repository methods

--- src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/repository/ScheduleRepository.java
+++ src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/repository/ScheduleRepository.java
@@ -1,11 +1,20 @@
 package com.danielsantanaribeiro.logusretailscheduleapi.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.danielsantanaribeiro.logusretailscheduleapi.model.Schedule;
 
 @Repository
-public interface ScheduleRepository extends JpaRepository<Schedule, Long>{
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+	List<Schedule> findByPatientNameAndScheduleDate(String patientName, LocalDate scheduleDate);
+
+	List<Schedule> findByPatientName(String name);
+	
+	List<Schedule> findByScheduleDate(LocalDate date);
 
 }
