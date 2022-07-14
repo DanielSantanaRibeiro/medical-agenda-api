8b41ecdd8d0a0f9482244022c69b9ff6b19196ff
Daniel Santana Ribeiro <danielsantanaribeiro2015@gmail.com>
2022-07-14 16:57:47 -0300

    Adding patients/{id} endpoint

--- src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/controller/PatientController.java
+++ src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/controller/PatientController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,6 +27,13 @@ public class PatientController {
 	@GetMapping
 	public ResponseEntity<List<Patient>> findAll(){
 		return ResponseEntity.ok().body(patientService.findAll());
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Patient> findById(@PathVariable Integer id){
+		Long idLong = Long.valueOf(id);
+		Patient patient = patientService.findById(idLong);
+		return ResponseEntity.ok().body(patient);
 	}
 	
 	@PostMapping
--- src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/services/PatientService.java
+++ src/main/java/com/danielsantanaribeiro/logusretailscheduleapi/services/PatientService.java
@@ -1,12 +1,14 @@
 package com.danielsantanaribeiro.logusretailscheduleapi.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.danielsantanaribeiro.logusretailscheduleapi.model.Patient;
 import com.danielsantanaribeiro.logusretailscheduleapi.repository.PatientRepository;
+import com.danielsantanaribeiro.logusretailscheduleapi.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PatientService {
@@ -20,4 +22,10 @@ public class PatientService {
 	public Patient save(Patient user) {
 		return patientRepository.save(user);		
 	}
+	
+	public Patient findById(Long id) {
+		Optional<Patient> obj = patientRepository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found for ID: " + id));
+	}
+
 }
